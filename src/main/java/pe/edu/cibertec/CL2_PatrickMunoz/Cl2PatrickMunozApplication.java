f95b5b299c074b46073d6ab5ba05311e02a0c26f
Patrickmo07 <patrick.3.mo@gmail.com>
2024-06-23 11:52:51 -0500

    First Commit

---
+++ src/main/java/pe/edu/cibertec/CL2_PatrickMunoz/Cl2PatrickMunozApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CL2_PatrickMunoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl2PatrickMunozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl2PatrickMunozApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/CL2_PatrickMunoz/Cl2PatrickMunozApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CL2_PatrickMunoz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl2PatrickMunozApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
